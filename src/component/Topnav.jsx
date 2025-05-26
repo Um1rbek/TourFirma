442e3f11bd698dc0a23fbdd327dbccc38eacbf47
Um1rbek <farxodovumar2@gmail.com>
2025-05-26 14:13:23 +0500

    commit

--- src/component/Topnav.jsx
+++ src/component/Topnav.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 const Topnav = () => {
   return (
-    <nav className="bg-gray-900 text-white w-full z-10 py-2">
-      <div data-aos="fade-up" className="container mx-auto flex justify-between items-center px-4">
-        <div className="text-xl font-bold">Tour Farm</div>
-        <div className="flex space-x-6">
-          <a href="#contacts" className="hover:text-gray-300">Контакты</a>
-          <a href="https://instagram.com" className="hover:text-gray-300">Instagram</a>
-          <a href="https://facebook.com" className="hover:text-gray-300">Facebook</a>
-          <a href="#ru" className="hover:text-gray-300">RU</a>
-        </div>
-      </div>
-    </nav>
+<nav className="fixed top-0 bg-gray-900 text-white w-full z-10 py-2">
+  <div data-aos="fade-up" className="container mx-auto flex justify-between items-center px-4">
+    <div className="text-xl font-bold">Tour Farm</div>
+    <div className="flex space-x-6">
+      <a href="#contacts" className="hover:text-gray-300">Контакты</a>
+      <a href="https://instagram.com" className="hover:text-gray-300">Instagram</a>
+      <a href="https://facebook.com" className="hover:text-gray-300">Facebook</a>
+      <a href="#ru" className="hover:text-gray-300">RU</a>
+    </div>
+  </div>
+</nav>
   );
 };
 
